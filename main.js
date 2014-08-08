d78c82cb18394057fe827a265bb8e7a8cb9f8d3d
Israel Bautista <bautista.israel@rocketmail.com>
2014-08-08 12:33:06 -0500

    functional app

--- main.js
+++ main.js
@@ -11,33 +11,23 @@ angular.module('myApp', [])
       // create variables to hold scope for meal price, tax rate and tip percentage directly from the Form
       var mealPrice = $scope.data.mealPrice;
       var taxRate = $scope.data.taxRate / 100;
-      var tipPercentage = $scope.data.tipPercentage / 100 * mealPrice;
+      var tip = $scope.data.tipPercentage / 100 * mealPrice;
 
       // calculate individual values for subtotal and total charges so it can be render on customer charges section
       var subTotal = mealPrice * taxRate + mealPrice;
-      var totalCharges = mealPrice + taxRate + tipPercentage;      
+     
 
-      // hold values for earnings info NOT WORKING
-      var addTipTotal = tipPercentage;
 
       // If the form is valid, provide the calculated values
-      if(validForm || validMealPrice || validTaxRate || validTipPercentage){
+      if(validForm && validMealPrice && validTaxRate && validTipPercentage){
         $scope.subTotal = subTotal;
-        $scope.tipPercentage = tipPercentage;
-        $scope.totalCharges = subTotal + tipPercentage;
-        $scope.tipTotal = addTipTotal;
+        $scope.totalCharges = tip + subTotal;      
+        $scope.tip = tip;
+        $scope.$broadcast('updateEarning', tip, subTotal);
       }
     };
 
-    // not working
-    $scope.mealCounter = function(){
-      var mealCount = 1;
-      this.mealCount = this.mealCount + 1;
-      return this.mealCount;
-    };
-
-    $scope.resetForm = function(){
-      if(confirm('are you sure?')){
+    $scope.resetForm = function(){      
         $scope.myForm.$setPristine();
         $scope.myForm.myMealPrice.$setPristine();
         $scope.myForm.myMealPrice.$error.number = '';
@@ -45,31 +35,50 @@ angular.module('myApp', [])
         $scope.myForm.myTaxRate.$error.number = '';
         $scope.myForm.myTipPercentage.$setPristine();
         $scope.myForm.myTipPercentage.$error.number = '';
-      }
     };
 
     $scope.startOver = function(){
-      if(confirm('are you sure?')){
-        $scope.myForm.$setPristine();
-        $scope.myForm.myMealPrice.$setPristine();
-        $scope.myForm.myMealPrice.$error.number = '';
-        $scope.myForm.myTaxRate.$setPristine();
-        $scope.myForm.myTaxRate.$error.number = '';
-        $scope.myForm.myTipPercentage.$setPristine();
-        $scope.myForm.myTipPercentage.$error.number = '';
-      }
+      $scope.myForm.$setPristine();
+      $scope.$broadcast('reset');
+      $scope.resetForm();
     };
+  })
 
+  .controller('ChargesCtrl', function($scope){
+    $scope.subTotal = 0.00;
+    $scope.tip = 0.00;
+    $scope.totalCharges = 0.00;
 
+    $scope.$on('reset', function(){
+      $scope.subTotal = 0.00;
+      $scope.tip = 0.00;
+      $scope.totalCharges = 0.00;
+    });
+
+    $scope.$on('updateEarning', function(evt, tip, subTotal){
+      $scope.subTotal = subTotal;
+      $scope.tip = tip;
+      $scope.totalCharges = tip + subTotal; 
+    });
+  })
+
+  
+  .controller('EarningsCtrl', function($scope){
+    $scope.tipTotal = 0.00;
+    $scope.mealCounter = 0;
+    $scope.averageTip = 0.00;
     
-  });
+    $scope.$on('reset', function(){
+      $scope.tipTotal = 0.00;
+      $scope.averageTip = 0.00;
+      $scope.mealCounter = 0;
+    });
 
+    $scope.$on('updateEarning', function(evt, tip){
+      $scope.tipTotal += tip;
+      $scope.mealCounter += 1;
+      $scope.averageTip = $scope.tipTotal / $scope.mealCounter;
+    });
+  })
 
-
-
-
-
-
-
-
-
+  
